fad4e1306a9696c4b2873ace2b9eec4a4fe2bd6d
Yerong Li <YerongLi>
2023-03-10 22:51:24 -0600

    warnup size 1 for retacred

--- scripts/run_large_retacred.sh
+++ scripts/run_large_retacred.sh
@@ -14,7 +14,7 @@ python3 src/run_prompt.py \
 --per_gpu_train_batch_size 2 \
 --gradient_accumulation_steps 1 \
 --max_seq_length 32 \
---warmup_steps 500 \
+--warmup_steps 1 \
 --learning_rate 3e-5 \
 --learning_rate_for_new_token 1e-5 \
 --num_train_epochs 5 \
