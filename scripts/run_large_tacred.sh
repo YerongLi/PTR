1de540e4a693be86a0f97f465eb15ccb08baf523
Yerong Li <YerongLi>
2023-03-10 21:42:01 -0600

    reduce warmup from 500 to 1

--- scripts/run_large_tacred.sh
+++ scripts/run_large_tacred.sh
@@ -13,8 +13,8 @@ python3 src/run_prompt.py \
 --model_name_or_path roberta-large \
 --per_gpu_train_batch_size 2 \
 --gradient_accumulation_steps 1 \
---max_seq_length 32 \
---warmup_steps 500 \
+--max_seq_length 16 \
+--warmup_steps 1 \
 --learning_rate 3e-5 \
 --learning_rate_for_new_token 1e-5 \
 --num_train_epochs 5 \
