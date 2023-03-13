17a6b6a5f5aaf5845b83c8ec0d4b2667dd9b3ef6
Yerong Li <YerongLi>
2023-03-12 22:19:06 -0500

    semeval

--- scripts/run_large_semeval.sh
+++ scripts/run_large_semeval.sh
@@ -4,7 +4,7 @@ for mode in train val test; do
     fi
 done
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=7
 
 python3 src/run_prompt.py \
 --data_dir data/semeval \
@@ -13,11 +13,11 @@ python3 src/run_prompt.py \
 --model_name_or_path roberta-large \
 --per_gpu_train_batch_size 1 \
 --gradient_accumulation_steps 1 \
---max_seq_length 32 \
+--max_seq_length 128 \
 --warmup_steps 500 \
 --learning_rate 3e-5 \
 --learning_rate_for_new_token 1e-5 \
---num_train_epochs 1 \
+--num_train_epochs 6 \
 --weight_decay 1e-2 \
 --adam_epsilon 1e-6 \
 --temps temp.txt
