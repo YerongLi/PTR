5361a6eb2e646cec031ee3d4392645ab702d573b
Yerong Li <YerongLi>
2023-03-15 04:07:16 -0500

    Test 46GB

---
+++ scripts/test_large_tacred2.sh
@@ -0,0 +1,23 @@
+for mode in train val test; do
+    if [ ! -d "data/retacred/$mode" ]; then
+        mkdir -p results/retacred/$mode
+    fi
+done
+
+export CUDA_VISIBLE_DEVICES=2,3
+
+python3 src/run_prompt.py \
+--data_dir data/retacred \
+--output_dir results/retacred2 \
+--model_type roberta \
+--model_name_or_path roberta-large \
+--per_gpu_train_batch_size 1 \
+--gradient_accumulation_steps 1 \
+--max_seq_length 512 \
+--warmup_steps 100 \
+--learning_rate 3e-5 \
+--learning_rate_for_new_token 1e-5 \
+--num_train_epochs 1 \
+--weight_decay 1e-2 \
+--adam_epsilon 1e-6 \
+--temps temp.txt
