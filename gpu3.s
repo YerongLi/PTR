473983e50e0ce6c12b310934a68598b1639d0e52
Yerong Li <yerong.li@outlook.com>
2023-03-15 03:09:37 -0500

    Upload all gpu configure views

---
+++ gpu3.s
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#SBATCH --time=00:01:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --job-name=myjob
+#SBATCH --partition=secondary
+#SBATCH --output=R-%x.%j.out
+#SBATCH --gres=gpu:TeslaT4
+##SBATCH --error=myjob.e%j
+##SBATCH --mail-user=yerong2@illinois.edu
+
+#
+# End of embedded SBATCH options
+#
+
+module load cuda/.11.1
+nvcc -V >> viewgpu3.log
+nvidia-smi >> vewgpu3.log
