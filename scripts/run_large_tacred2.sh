26f64c4bbfc5d74c3d7c98a8c2a3bc2baba1e015
Yerong Li <YerongLi>
2023-03-15 04:33:37 -0500

    Reset to original

--- scripts/run_large_tacred2.sh
+++ scripts/run_large_tacred2.sh
@@ -14,10 +14,10 @@ python3 src/run_prompt.py \
 --per_gpu_train_batch_size 1 \
 --gradient_accumulation_steps 1 \
 --max_seq_length 512 \
---warmup_steps 100 \
+--warmup_steps 500 \
 --learning_rate 3e-5 \
 --learning_rate_for_new_token 1e-5 \
---num_train_epochs 1 \
+--num_train_epochs 5 \
 --weight_decay 1e-2 \
 --adam_epsilon 1e-6 \
 --temps temp.txt
