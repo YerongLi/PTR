68d857e9bbb39fd95d489ed361d1ca51dbc7e926
Yerong Li <YerongLi>
2023-03-16 02:21:40 -0500

    Pass log into progress_bar_log()

--- src/utils.py
+++ src/utils.py
@@ -1,24 +1,23 @@
-# log.addHandler(TqdmLoggingHandler())
+# class TqdmLoggingHandler(logging.Handler):
+#     def __init__(self, level=logging.NOTSET):
+#         super().__init__(level)
 
-class TqdmLoggingHandler(logging.Handler):
-    def __init__(self, level=logging.NOTSET):
-        super().__init__(level)
-
-    def emit(self, record):
-        try:
-            msg = self.format(record)
-            tqdm.tqdm.write(msg)
-            self.flush()
-        except Exception:
-            self.handleError(record)
+#     def emit(self, record):
+#         try:
+#             msg = self.format(record)
+#             tqdm.tqdm.write(msg)
+#             self.flush()
+#         except Exception:
+#             self.handleError(record)
 class progress_bar_log():
     def __init__(self, log):
         self.length = 6
         self.bars = { k : None for k in range(self.length)}
+        self.log = log
+    
     def check(self, i, total):
-
         progress = int(i/total*len(self.bars)) % len(self.bars)
         if (progress in self.bars): 
                 del self.bars[progress]
-                log.info(f'{progress}/{self.length}')
+                self.log.info(f'{progress}/{self.length}')
 
