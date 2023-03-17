4a4a09b3d53cccfe29dcf61b5604dd67282c7e65
Yerong Li <YerongLi>
2023-03-17 00:47:11 -0500

    Try batch size 6

--- scripts/run_large_tacred.sh
+++ scripts/run_large_tacred.sh
@@ -11,7 +11,7 @@ python3 src/run_prompt.py \
 --output_dir results/tacred \
 --model_type roberta \
 --model_name_or_path roberta-large \
---per_gpu_train_batch_size 2 \
+--per_gpu_train_batch_size 6 \
 --gradient_accumulation_steps 1 \
 --max_seq_length 512 \
 --warmup_steps 500 \
