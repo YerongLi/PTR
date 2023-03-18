d4e9f34dd5127ed9017e696f6354c6585dde9dde
Yerong Li <YerongLi>
2023-03-17 22:04:38 -0500

    Update

--- submit.s
+++ submit.s
@@ -6,7 +6,7 @@
 #SBATCH --job-name=myjob
 #SBATCH --partition=secondary
 #SBATCH --output=ptrlog.log
-#SBATCH --gres=gpu:V100
+#SBATCH --gres=gpu:V100:1
 ##SBATCH --error=myjob.e%j
 ##SBATCH --mail-user=yerong2@illinois.edu
 
@@ -14,6 +14,6 @@
 # End of embedded SBATCH options
 #
 
-module load cuda/10.0
+module load cuda/.11.6
 conda activate /scratch/yerong/.conda/envs/ptr
 bash scripts/run_large_retacred.sh > ptr.log
