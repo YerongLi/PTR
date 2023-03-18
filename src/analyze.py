cca4d2c6b2c3f0bc9c489817a12fd1238d808cb1
Yerong Li <YerongLi>
2023-03-17 23:18:23 -0500

    Log the range of all_labels and predictions

--- src/analyze.py
+++ src/analyze.py
@@ -121,11 +121,13 @@ test_dataset = REPromptDataset.load(
     temps = temps,
     tokenizer = tokenizer,
     rel2id = args.data_dir + "/" + "rel2id.json")
-log.info(test_dataset.rel2id)
+# log.info(test_dataset.rel2id)
 predictions = np.argmax(scores, axis=1)
-log.info(f'all_labels[:50]] {all_labels[:50]}')
-log.info(f'predictions[:50]] {predictions[:50]}')
+# log.info(f'all_labels[:50]] {all_labels[:50]}')
+# log.info(f'predictions[:50]] {predictions[:50]}')
 cm = confusion_matrix(all_labels, predictions)
+log.info(sorted(list(set(all_labels))))
+log.info(sorted(list(set(predictions))))
 log.info(f'cm.shape {cm.shape}')
 rel2idlist = [test_dataset.rel2id for k in range(len(test_dataset.rel2id))]
 disp = ConfusionMatrixDisplay(confusion_matrix=cm,
