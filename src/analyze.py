631cb9f2ce878aa3a2196d827350615039b9a63e
Yerong Li <YerongLi>
2023-03-19 01:24:52 -0500

    key should be labels not label

--- src/analyze.py
+++ src/analyze.py
@@ -148,7 +148,8 @@ for i, data in enumerate(test_dataset):
     log.info(i)
     input_ids = [t for t in data['input_ids'] if t != tokenizer.pad_token_id]
     log.info(tokenizer.decode(input_ids, skip_special_tokens=False))
-    log.info(rel2idlist[data['labels'][0]])
+    log.info(rel2idlist[data['labels'].numpy())
+    log.info(rel2idlist[data['labels'].numpy().[0]])
     log.info(rel2idlist[predictions[i]])
     if i > 5: break
 log.info('End of script')
