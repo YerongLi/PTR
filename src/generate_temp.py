cc6fc1e8850da07efa745e8b28a8a39a07a76c09
Yerong <yerong.li@outlook.com>
2023-04-06 17:55:35 -0500

    Run from the home directory

--- src/generate_temp.py
+++ src/generate_temp.py
@@ -2,7 +2,7 @@ import json
 from templating import get_temps
 oldtemp = open('data/retacred/temp.txt', 'r')
 newtemp = open('data/retacred/temp.1.txt', 'w')
-testfile = json.loads(open('data/retacred/test.txt', 'w'))
+testfile = json.loads(open('data/retacred/test.txt', 'w').read())
 for i in oldtemp.readlines():
     entries = i.strip().split("\t")
     if entries[2] == 'per:':
