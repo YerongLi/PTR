e3e027ae40616a5275db77a7ff14791056384ad5
Yerong <yerong.li@outlook.com>
2023-04-06 18:04:41 -0500

    Remove the global features

--- src/generate_temp.py
+++ src/generate_temp.py
@@ -2,15 +2,15 @@ import json
 from templating import get_temps
 oldtemp = open('data/retacred/temp.txt', 'r')
 newtemp = open('data/retacred/temp.1.txt', 'w')
-if features is None:
-    self.args = get_args()
-    with open(path+"/" + name, "r") as f:
-        features = []
-        for line in f.readlines():
-            line = line.rstrip()
-            if len(line) > 0:
-            	print(dir(eval(line)))
-                # features.append(eval(line))  
+
+self.args = get_args()
+with open(path+"/" + name, "r") as f:
+    features = []
+    for line in f.readlines():
+        line = line.rstrip()
+        if len(line) > 0:
+        	print(dir(eval(line)))
+            # features.append(eval(line))  
 for i in oldtemp.readlines():
     entries = i.strip().split("\t")
     if entries[2] == 'per:':
