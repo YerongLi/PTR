1957e4458a70aaf5a5a63dd619601a6d038943f3
Yerong <yerong.li@outlook.com>
2023-04-06 18:20:30 -0500

    Print relation and token

--- src/generate_temp.py
+++ src/generate_temp.py
@@ -4,17 +4,19 @@ oldtemp = open('data/retacred/temp.txt', 'r')
 newtemp = open('data/retacred/temp.1.txt', 'w')
 path =  'data/retacred/test.txt'
 print('start the loop')
+candidate = {'per:employee_of', 'per:title', 'org:country_of_branch', 'per:city_of_death'}
+candidate = {c : list() for c in cands}
 with open(path, "r") as f:
     features = []
     for line in f.readlines():
         line = line.rstrip()
-
         if len(line) > 0:
         	data = eval(line)
-        	print(data['h'])
-        	print(data['token'])
+        	if data['relation'] in candidate:
+        		candidate.append((data['h']['name'], data['t']['name']))
             # features.append(eval(line))  
 for i in oldtemp.readlines():
     entries = i.strip().split("\t")
-    if entries[2] == 'per:':
+    if entries[2] in candidate:
+    	print(entries[2])
     	continue
