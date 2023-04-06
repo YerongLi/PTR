32173f109820800ffe24a506e7b2aa9a713c26aa
Yerong <yerong.li@outlook.com>
2023-04-06 18:08:04 -0500

    Print some hint before the loop

--- src/generate_temp.py
+++ src/generate_temp.py
@@ -3,7 +3,7 @@ from templating import get_temps
 oldtemp = open('data/retacred/temp.txt', 'r')
 newtemp = open('data/retacred/temp.1.txt', 'w')
 path =  'data/retacred/test.txt'
-
+print('start the loop')
 with open(path, "r") as f:
     features = []
     for line in f.readlines():
