8b21c2ba9ec16d65c15475a0ad39065db8ce4ed4
Yerong <yerong.li@outlook.com>
2023-04-16 01:44:37 -0500

    Print ['per:country_of_death']

--- src/data_prompt.py
+++ src/data_prompt.py
@@ -145,12 +145,7 @@ class REPromptDataset(DictDataset):
                 self.temp_ids[name]['label_ids'][j] = self.set[j].index(
                     self.temp_ids[name]['label_ids'][j])
         logging.info('selftemp_ids')
-        logging.info(self.temp_ids)
-        for k in self.temp_ids['per:country_of_death']:
-            logging.info(f'    {k}')
-            for item in self.temp_ids['per:country_of_death'][k]:
-                logging.info(item)
-                logging.info(tokenizer(item))
+        logging.info(self.temp_ids['per:country_of_death'])
         self.prompt_id_2_label = torch.zeros(len(self.temp_ids), len(self.set)).long()
         
         for name in self.temp_ids:
