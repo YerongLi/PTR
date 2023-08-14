7a6052f7c512531e911c92eca81e17d0e14e8b4e
Yerong <yerong.li@outlook.com>
2023-08-13 20:38:45 -0500

    [77.4, 85.6, 0]

--- tmp/scale.py
+++ tmp/scale.py
@@ -8,7 +8,7 @@ def scale_pair(pair):
     return scaled_large, scaled_small
 
 def main():
-    baseline = np.array([78.0 85.3, 0])
+    baseline = np.array([77.4, 85.6, 0])
     prob = 0.8
     
     compare = np.array([(89.49, 88.00), (90.66, 91.08), (93.52, 90.90)])
